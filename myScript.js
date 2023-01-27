93e0ebb2696ebc2e8f88a53d4e2a4702c6bdeb87
spider-Monty <montyaction@hotmail.com>
2023-01-27 19:07:03 +0530

    Create Hoisting

---
+++ myScript.js
@@ -0,0 +1,23 @@
+// getName();
+// // console.log(a);
+
+// function getName() {
+//     console.log("montyaction")
+// }
+
+// console.warn("finished!a")
+
+console.log(sum(2, 3));
+console.log(city);
+console.log(info);
+
+function sum(x, y) { return x + y  };
+
+const names = "montyaction";
+
+let info = {
+    age : 30,
+    nationality : "India"
+}
+
+var city = "Green City";
